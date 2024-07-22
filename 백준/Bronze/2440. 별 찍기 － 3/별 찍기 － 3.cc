13a588a509cefc42645c6baa0fa994afd157d9a9
JiHxxn-zip <kda2443@gmail.com>
2024-07-22 16:52:06 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iSelectNum(0);
+
+    cin >> iSelectNum;
+
+	for (int i = 0; i < iSelectNum; ++i)
+	{
+		for (int j = 0; j < iSelectNum; ++j)
+		{
+			if (iSelectNum -1 >= i+j)
+			{
+				cout << "*";
+			}
+		}
+		cout << endl;
+	}
+
+    return 0;
+}
