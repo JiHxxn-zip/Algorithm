8c14c490bece19fe7be6528e8541343536d5a0ad
JiHxxn-zip <kda2443@gmail.com>
2024-07-28 23:12:49 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iA(0), iB(0);
+    cin >> iA;
+    cin >> iB;
+
+    cout << iA + iB << endl << iA - iB << endl << iA * iB << endl << iA / iB << endl << iA % iB;
+
+    return 0;
+}
