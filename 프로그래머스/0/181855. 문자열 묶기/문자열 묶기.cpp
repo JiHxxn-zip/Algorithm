f9f6bba31060430217fe0acae7a6df9c920cf9fe
JiHxxn-zip <kda2443@gmail.com>
2024-11-24 21:36:37 +0900

    [level 0] Title: 문자열 묶기, Time: 5.43 ms, Memory: 15.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<string> strArr) {
+    vector<int> len(31, 0);
+    int answer = 0;
+    
+    for(int i = 0; i < strArr.size(); ++i) {
+        len[strArr[i].size()]++;
+        answer = max(answer, len[strArr[i].size()]);
+    }
+    return answer;
+}
