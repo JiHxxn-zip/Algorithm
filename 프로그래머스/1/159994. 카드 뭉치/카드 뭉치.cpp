155b335c65026afdd91fbe065e3c0336fa5ce7c1
JiHxxn-zip <kda2443@gmail.com>
2025-06-24 10:19:11 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+string answer = "";
+
+	int idxA = 0;
+	int idxB = 0;
+	for (int i = 0; i < goal.size(); i++)
+	{
+		if (cards1[idxA] == goal[i])
+		{
+			idxA++;
+		}
+		else if (cards2[idxB] == goal[i])
+		{
+			idxB++;
+		}
+		else
+		{
+			return "No";
+		}
+	}
+
+	return "Yes";
+}
