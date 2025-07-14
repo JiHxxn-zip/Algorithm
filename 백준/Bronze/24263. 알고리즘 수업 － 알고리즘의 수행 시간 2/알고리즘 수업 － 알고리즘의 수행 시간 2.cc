18f3ae79c356670be3a12dee856e46c39dbe25c8
JiHxxn-zip <kda2443@gmail.com>
2025-07-15 08:54:03 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int n;
+ 
+    cin >> n; //C언어 : scanf("%d",&n);
+ 
+    cout << n << '\n' << 1 << '\n'; //C언어 : printf("%d\n%d", n, 1);
+ 
+    return 0;
+}
