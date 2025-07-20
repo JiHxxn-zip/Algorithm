ff0f00ccc76cb4357c256d1f5699f30e18e89b7d
JiHxxn-zip <kda2443@gmail.com>
2025-07-20 09:52:37 +0900

    [Bronze I] Title: 소인수분해, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int N;
+	cin >> N;
+
+	int i = 2;
+	
+	while(N!=1)
+	{
+		if (N % i == 0) 
+        {
+			cout << i << "\n";
+			N /= i;
+		}
+		else { i++; }
+	}
+}
