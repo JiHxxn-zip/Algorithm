37c6257c384a0fe8aaafc93a59db176896a0fe2e
JiHxxn-zip <kda2443@gmail.com>
2025-07-22 09:03:31 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+	int result=2;
+
+	int repeat;
+	cin >> repeat;
+
+	while (repeat--) result = (result * 2) - 1;
+
+	cout << result * result;
+}
