1e984167981d31e941847195f1b5ca04f0a61ed3
JiHxxn-zip <kda2443@gmail.com>
2025-09-16 13:08:27 +0900

    [level 2] Title: 가장 큰 수, Time: 59.36 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -4,28 +4,31 @@
 
 using namespace std;
 
-string solution(vector<int> numbers) {
-     // vec<string> 안에 먼저 넣어줌
-  vector<string> vec_str;
-  vec_str.reserve(numbers.size());
+string solution(vector<int> numbers) 
+{
+     // str으로 형변환해서 vec에 담아줌
+    vector<string> vec_str;
 
-  for (size_t i = 0; i < numbers.size(); i++)
-      vec_str.push_back(to_string(numbers[i]));
+    vec_str.reserve(numbers.size());
 
-  // 정렬을 해줄건데, 컴퍼레이터 함수를 만들어줄거임.
-  sort(vec_str.begin(), vec_str.end(),
-      [](const string& a, const string& b) -> bool
-      {
-          return a + b > b + a;
-      });
+    for (size_t i = 0; i < numbers.size(); i++)
+        vec_str.push_back(to_string(numbers[i]));
 
-  string answer = "";
-  
-  for (string str : vec_str)
-      answer += str;
+    // sort에서 컴퍼레이터 함수를 만들어서 정렬 시도
+    // 조건은 a+b > b+a 일 경우만 정렬하도록
+    sort(vec_str.begin(), vec_str.end(),
+    [](const string& a, const string& b) -> bool
+    {
+        return a + b > b + a;
+    });
 
-  if (answer.empty() || answer[0] == '0')
-      answer = "0";
+    string answer = "";
 
-  return answer;
+    for (string str : vec_str)
+        answer += str;
+    // 0인 예외처리
+    if (answer[0] == '0' || answer.empty())
+        answer = "0";
+
+    return answer;
 }
