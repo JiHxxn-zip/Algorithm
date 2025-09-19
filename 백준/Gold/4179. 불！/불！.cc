2ca4c7020269ca1b15cee2a80110415c896e7bfd
JiHxxn-zip <kda2443@gmail.com>
2025-09-19 12:36:22 +0900

    [Gold III] Title: 불!, Time: 44 ms, Memory: 10940 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,111 +1,122 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include "algorithm"
 using namespace std;
 
-void BFS(queue<pair<int, int>> q_Ji, queue<pair<int, int>> q_Fire);
+string board[1001];
+int f_Dist[1001][1001]; // 불 dist
+int j_Dist[1001][1001]; // 지훈 disg
 
-string strBoard[1002];
-int iDist[1002][1002];
-int iDistFire[1002][1002];
+int dx[4]{ 0, 0, -1, 1 };
+int dy[4]{ 1, -1, 0, 0 };
 
-int dx[4]{ 1, 0, -1, 0 };
-int dy[4]{ 0, 1, 0 ,-1 };
-
-int n(0), m(0);
-
-int main()
+int main(void)
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	queue<pair<int, int>> q_Ji;
-	queue<pair<int, int>> q_Fire;
+    // 행과 열을 저장해줄 변수
+    int row, col;
 
-	cin >> n >> m;
+    cin >> row >> col;
 
-	for (int i = 0; i < n; ++i)
-	{
-		fill(iDist[i], iDist[i] + m, -1);
-		fill(iDistFire[i], iDistFire[i] + m, -1);
-	}
+    // 갈 수 없는 지역 셋팅
+    for (size_t i = 0; i < row; i++)
+    {
+        fill(f_Dist[i], f_Dist[i] + col, -1);
+        fill(j_Dist[i], j_Dist[i] + col, -1);
+    }
 
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> strBoard[i];
+    // Board 셋팅
+    for (size_t i = 0; i < row; i++)
+        cin >> board[i];
 
-		for (int j = 0; j < m; ++j)
-		{
-			if (strBoard[i][j] == 'J')
-			{
-				q_Ji.push({ i,j });	// 지훈의 시작점을 저장
-				iDist[i][j] = 0;
-			}
+    // 갈 수 있는 위치를 저장해주는 q
+    queue<pair<int, int>> q;
+    for (size_t i = 0; i < row; i++)
+        for (size_t j = 0; j < col; j++)
+            if (board[i][j] == 'F')
+            {
+                q.push({ i, j });
+                f_Dist[i][j] = 0;
+            }
+    
+    // 불 BFS 시작
+    while (!q.empty())
+    {
+        pair<int, int> cur = q.front();
+        q.pop();
 
-			if (strBoard[i][j] == 'F')
-			{
-				q_Fire.push({ i,j });	// 불의 시작점을 저장
-				iDistFire[i][j] = 0;
-			}
-		}
-	}
+        for (size_t i = 0; i < 4; i++)
+        {
+            int nx = cur.second + dx[i];
+            int ny = cur.first + dy[i];
 
-	BFS(q_Ji, q_Fire);
+            if (nx < 0 || nx >= col || ny < 0 || ny >= row)
+                continue;
+            if (f_Dist[ny][nx] >= 0 || board[ny][nx] == '#')
+                continue;
 
-	return 0;
-}
+            f_Dist[ny][nx] = f_Dist[cur.first][cur.second] + 1;
+            q.push({ ny, nx });
+        }
+    }
 
-void BFS(queue<pair<int, int>> q_Ji, queue<pair<int, int>> q_Fire)
-{
-	// 불 BFS
-	while (!q_Fire.empty())
-	{
-		pair<int, int> FTemp = q_Fire.front();
-		q_Fire.pop();
-		for (int i = 0; i < 4; ++i)
-		{
-			int mx = FTemp.first + dx[i];
-			int my = FTemp.second + dy[i];
+    for (size_t i = 0; i < row; i++)
+        for (size_t j = 0; j < col; j++)
+            if (board[i][j] == 'J')
+            {
+                q.push({ i, j });
+                j_Dist[i][j] = 0;
+            }
 
-			if (mx < 0 || mx >= n || my < 0 || my >= m)
-				continue;
+    // 탈출 위치 저장
+    pair<int, int> exit;
 
-			if (iDistFire[mx][my] >= 0 || strBoard[mx][my] == '#')
-				continue;
+    // 지훈 BFS 시작
+    while (!q.empty())
+    {
+        pair<int, int> cur = q.front();
+        q.pop();
+    
+        for (size_t i = 0; i < 4; i++)
+        {
+            int nx = cur.second + dx[i];
+            int ny = cur.first + dy[i];
 
-			iDistFire[mx][my] = iDistFire[FTemp.first][FTemp.second] + 1;
-			q_Fire.push({ mx, my });
-		}
-	}
+            if (nx < 0 || nx >= col || ny < 0 || ny >= row)
+            {
+                cout << j_Dist[cur.first][cur.second] + 1;
+                return 0;
+            }
+    
+            if (j_Dist[ny][nx] >= 0 || board[ny][nx] == '#')
+                continue;
+            int nextTime = j_Dist[cur.first][cur.second] + 1;
 
-	// 지훈 BFS
-	while (!q_Ji.empty())
-	{
-		pair<int, int> temp = q_Ji.front();
-		q_Ji.pop();
+            // 지훈이가 가려는 위치에 불이 존재하고, 불이 먼저왔다면(숫자가 작다면)
+            if (f_Dist[ny][nx] != -1 && f_Dist[ny][nx] <= nextTime)
+                continue;
 
-		for (int i = 0; i < 4; ++i)
-		{
-			int mx = temp.first + dx[i];
-			int my = temp.second + dy[i];
+            j_Dist[ny][nx] = j_Dist[cur.first][cur.second] + 1;
+            q.push({ ny, nx });
+        }
+    }
 
-			if (mx < 0 || mx >= n || my < 0 || my >= m)
-			{
-				// 탈출 성공
-				int result = iDist[temp.first][temp.second];
-				cout << result + 1;
-				return;
-			}
+    cout << "IMPOSSIBLE";
 
-			if (iDist[mx][my] >= 0 ||  strBoard[mx][my] == '#')
-				continue;
+    return 0;
+}       
 
-			if (iDistFire[mx][my] != -1 && iDistFire[mx][my] <= iDist[temp.first][temp.second] + 1)
-				continue;
+// 거리 측정이 필요한 BFS 문제
+// 예외처리가 좀 다르게 가장자리로 가야 탈출.
+// 벽이 있는 곳은 갈 수 없음
+// 사이즈는 1000
+// 벽은 #, J는 초기 위치, F는 불, .은 이동 가능위치
+// 행, 열, string board
 
-			iDist[mx][my] = iDist[temp.first][temp.second] + 1;
-			q_Ji.push({ mx, my });
-		}
-	}
-
-	cout << "IMPOSSIBLE";
-}
+// 서로 상호 작용하는 조건은 없기 때문에 불의 dist와 지훈의 dist를 구해서 비교
+// 지훈이를 결국 탈출 시켜야하기 때문에 불의 dist를 먼저 다 구하고 
+// 지훈이의 BFS에서 불의 dist를 가지고 놀면 될듯
