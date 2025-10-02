6b85c3485c54d8e2750575f38f3fc19bc2dd7f69
JiHxxn-zip <kda2443@gmail.com>
2025-10-02 14:50:58 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,51 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <climits>
-using namespace std;
+    using namespace std;
+    #include <iostream>
+    #include <algorithm>
+    #include <vector>
 
-int s[305], d[305];
+    int a[301];
+    int b[301];
 
-
-int main(void)
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int n;
-    cin >> n;
-
-    int tot = 0;
-
-    for (int i = 1; i <= n; i++) 
+    int main() 
     {
-        cin >> s[i];
-        tot += s[i];
-    }
+        ios::sync_with_stdio(false);
+        cin.tie(0);
 
-    if (n == 1) 
-    {
-        cout << s[1];
+        int n{}, tot{};
+        cin >> n;
+
+        for (size_t i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            tot += a[i];
+        }
+
+        if (n == 1 || n == 2)
+        {
+            cout << tot;
+            return 0;
+        }
+
+
+        /*b[0] = a[0];
+        b[1] = a[1];
+        b[2] = a[2];
+
+        for (size_t i = 3; i < n; i++)
+            b[i] = min(b[i-2], b[i-3]) + a[i];*/
+
+        b[0] = a[0];
+        b[1] = a[0] + a[1];
+
+        for (size_t i = 2; i < n; i++)
+            b[i] = max(b[i-2], b[i-3] + a[i-1]) + a[i];
+
+        cout << b[n - 1];
+
         return 0;
     }
-    else if (n == 2) 
-    {
-        cout << s[1] + s[2];
-        return 0;
-    }
-
-    // 초기 셋팅
-    d[1] = s[1];
-    d[2] = s[2];
-    d[3] = s[3];
-
-    // 4부터 시작
-    // 점화식 : 연속 3칸 제한 때문에 i에 오려면 i-2나 i-3에서 올 수 있음
-    for (int i = 4; i <= n; i++) 
-        d[i] = min(d[i - 2], d[i - 3]) + s[i];
-
-    // 꼭대기 1, 2칸 전의 값 중 작은 값을 total 점수에서 빼준 값이 정답
-    cout << tot - min(d[n - 1], d[n - 2]);
-
-    return 0;
-}
