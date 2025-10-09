9e5eeeb6c3ec5e7112804aba79627f8fb81c1316
JiHxxn-zip <kda2443@gmail.com>
2025-10-09 17:46:47 +0900

    [Gold IV] Title: 세 수의 합, Time: 52 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    sort(a.begin(), a.end());
+
+    vector<int> two;
+    two.reserve(n*n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            two.push_back(a[i] + a[j]);
+    }
+
+    sort(two.begin(), two.end());
+
+    for (int i = n-1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (binary_search(two.begin(), two.end(), a[i] - a[j]))
+            {
+                cout << a[i];
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
