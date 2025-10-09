4c23917e1b65c0fda0524a54544ad9db6db2e40b
JiHxxn-zip <kda2443@gmail.com>
2025-10-09 10:54:45 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -8,29 +8,20 @@ int main(void)
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n{};
+
+    int n;
     cin >> n;
-
-    vector<int> w;
-    w.reserve(n);
-
-    int k;
+    
+    vector<int> vec(n);
     for (int i = 0; i < n; i++)
-    {
-        cin >> k;
-        w.push_back(k);
-    }
-
-    sort(w.begin(), w.end());
+        cin >> vec[i];
+    sort(vec.begin(), vec.end());
 
     int answer{};
     for (int i = 0; i < n; i++)
-        answer = max(answer, w[i] * (n - i));
+        answer = max(answer, vec[i] * (n - i));
 
     cout << answer;
 
     return 0;
 }
-
-// 로프의 힘을 균등하게 배분하려면 최소의 힘을 내는 로프를 시작으로 값을 비교해 가면 됨
