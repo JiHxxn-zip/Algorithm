7d1763e1e8474a3baefb074979b7dd75c540c918
JiHxxn-zip <kda2443@gmail.com>
2025-10-09 11:50:37 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -8,36 +8,24 @@ int main(void)
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n{}, k{};
+
+    int n , k;
     cin >> n >> k;
+ 
+    vector<pair<int, int>> vec(n);
+    for (int i = 0; i < n; i++) 
+        cin >> vec[i].first >> vec[i].second; 
 
-    vector<int> w, v;
-    w.reserve(n + 1);
-    v.reserve(n + 1);
-
-    int w_t{}, v_t{};
-    for (int i = 0; i < n; i++)
+    vector<int> dp(k + 1, 0);
+    for (int i = 0; i < n; i++) 
     {
-        cin >> w_t >> v_t;
-        w.push_back(w_t);
-        v.push_back(v_t);
-    }
-
-    vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= k; j++)
+        int w = vec[i].first, v = vec[i].second;
+        for (int j = k; j >= w; j--) 
         {
-            if (j < w[i - 1])  // 무게 초과 시
-                dp[i][j] = dp[i - 1][j];
-            else               // 아이템을 담을 수 있을 때
-                dp[i][j] = max(dp[i - 1][j], v[i - 1] + dp[i - 1][j - w[i - 1]]);
+            dp[j] = max(dp[j], dp[j - w] + v);
         }
     }
-
-    cout << dp[n][k];
-
+    
+    cout << dp[k];
     return 0;
 }
-
