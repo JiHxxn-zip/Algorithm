0075e81e01132b58edc8d0986302cecf612d481f
JiHxxn-zip <kda2443@gmail.com>
2025-10-10 11:58:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 284 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -4,71 +4,26 @@
 #include <climits>
 using namespace std;
 
-int n, m;
-vector<int> vec;
-
-int lower_idx(int target);
-int upper_idx(int target);
-
 int main(void)
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
+    int n, m;
     cin >> n;
-    vec.reserve(n);
 
-    int x;
-    for (int i = 0; i < n; i++) 
-    {
-        cin >> x;
-        vec.push_back(x); 
-    }
-    sort(vec.begin(), vec.end());
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
 
+    sort(a.begin(), a.end());
     cin >> m;
+    vector<int> b(m);
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
 
     for (int i = 0; i < m; i++)
-    {
-        int t;
-        cin >> t;
+        cout << upper_bound(a.begin(), a.end(), b[i]) - lower_bound(a.begin(), a.end(), b[i]) << ' ';
 
-        cout << upper_idx(t) - lower_idx(t) << ' ';
-    }
     return 0;
-}
-
-int lower_idx(int target)
-{
-    int st = 0;
-    int en = n;
-
-    while (st < en)
-    {
-        int mid = (en + st) / 2;
-
-        if (vec[mid] >= target)
-            en = mid;
-        else
-            st = mid + 1;
-    }
-
-    return st;
-}
-int upper_idx(int target)
-{
-    int st = 0;
-    int en = n;
-
-    while (st < en)
-    {
-        int mid = (en + st) / 2;
-
-        if (vec[mid] > target)
-            en = mid;
-        else
-            st = mid + 1;
-    }
-
-    return st;
 }
