1ed56e7cdd60ca9c78c9394f6ef2bd448cd1c860
JiHxxn-zip <kda2443@gmail.com>
2025-10-10 14:26:08 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int mn = INT_MAX;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, s;
+    cin >> n >> s;
+
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+
+    int total = vec[0];
+    int en{};
+
+    for (int i = 0; i < n; i++)
+    {
+        while (en < n && total < s)
+        {
+            en++;
+            if (en != n)
+                total += vec[en];
+        }
+        
+        if (en == n)
+            break;
+
+        mn = min(mn, en - i + 1);
+        total -= vec[i];
+    }
+
+    if (mn == INT_MAX)
+        mn = 0;
+
+    cout << mn;
+    return 0;
+}
