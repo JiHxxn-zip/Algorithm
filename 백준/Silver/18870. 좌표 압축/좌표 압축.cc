cd3101006f380dfed88681a7da4f52f80b920f44
JiHxxn-zip <kda2443@gmail.com>
2025-10-10 12:05:18 +0900

    [Silver II] Title: 좌표 압축, Time: 436 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -4,28 +4,26 @@
 #include <climits>
 using namespace std;
 
-int x[1000005];
 int main(void)
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
+    int n, m;
     cin >> n;
 
-    vector<int> uni;
-    uni.reserve(n);
+    vector<int> a(n), b(n);
     for (int i = 0; i < n; i++)
     {
-        cin >> x[i];
-        uni.push_back(x[i]);
+        cin >> a[i];
+        b[i] = a[i];
     }
- 
-    sort(uni.begin(), uni.end());
-    uni.erase(unique(uni.begin(), uni.end()), uni.end());
+
+    sort(a.begin(), a.end());
+    a.erase(unique(a.begin(), a.end()), a.end());
 
     for (int i = 0; i < n; i++)
-        cout << lower_bound(uni.begin(), uni.end(), x[i]) - uni.begin() << ' ';
-    
+        cout << lower_bound(a.begin(), a.end(), b[i]) - a.begin() << ' ';
+
     return 0;
 }
