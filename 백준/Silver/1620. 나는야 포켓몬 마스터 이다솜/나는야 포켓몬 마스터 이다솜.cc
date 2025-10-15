33a0590da9033d51bb37287e4082da2cb3000f9f
JiHxxn-zip <kda2443@gmail.com>
2025-10-15 21:14:21 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 92 ms, Memory: 20664 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,37 @@
+using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    
+    unordered_map<string, int> s2i;
+    vector<string> i2s(n+1);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> i2s[i];
+        s2i[i2s[i]] = i;
+    }
+
+    while (m--)
+    {
+        string query;
+        cin >> query;
+
+        if (isdigit(query[0]))
+            cout << i2s[stoi(query)] << '\n';
+        else
+            cout << s2i[query] << '\n';
+    }
+
+    return 0;
+}
