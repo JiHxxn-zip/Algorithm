2d1e3d927557a2280f52682e5e09109624194640
JiHxxn-zip <kda2443@gmail.com>
2025-10-29 20:25:32 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,40 @@
+using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    priority_queue<int, vector<int>, greater<int>>pq;
+
+    int x;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        if (x == 0)
+        {
+            if (pq.empty())
+            {
+                cout << 0 << '\n';
+                continue;
+            }
+
+            cout << pq.top() << '\n';
+            pq.pop();
+        }
+        else
+        {
+            pq.push(x);
+        }
+    }
+
+    return 0;
+}
