91c13f6fdce9bbb8257a7890c1ac2dc04c2383ec
JiHxxn-zip <kda2443@gmail.com>
2025-11-01 11:06:28 +0900

    [Gold II] Title: 보석 도둑, Time: 220 ms, Memory: 18496 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -13,37 +13,35 @@ int main()
     int n, k;
     cin >> n >> k;
 
-    vector<pair<int, int>>vec(n);
+    vector<pair<int, int>> vec(n);
 
+    // 보석
     for (int i = 0; i < n; i++)
-    {
         cin >> vec[i].second >> vec[i].first;
-    }
+    
     sort(vec.rbegin(), vec.rend());
 
     multiset<int> bag;
-    int c;
+    // 가방
     for (int i = 0; i < k; i++)
     {
-        cin >> c;
-        bag.insert(c);
+        int q;
+        cin >> q;
+        bag.insert(q);
     }
 
     long long answer{};
-
-    for (auto jweInfo : vec)
+    for (auto a : vec)
     {
-        auto it = bag.lower_bound(jweInfo.second);
+        auto it = bag.lower_bound(a.second);
         if (it == bag.end())
             continue;
 
-        answer += jweInfo.first;
+        answer += a.first;
         bag.erase(it);
     }
 
     cout << answer << '\n';
+
     return 0;
 }
-
-// 크기가 가장 작은 가방부터
-// - 자신이 담을 수 있는 가장 비싼 보석부터 담기
