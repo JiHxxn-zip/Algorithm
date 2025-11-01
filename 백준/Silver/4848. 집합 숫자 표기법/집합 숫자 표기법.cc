3a97571a520fe9d8a2da6033f4b856f39443025f
JiHxxn-zip <kda2443@gmail.com>
2025-11-01 12:43:23 +0900

    [Silver II] Title: 집합 숫자 표기법, Time: 4 ms, Memory: 2876 KB -BaekjoonHub

--- 백준/Silver/4848. 집합 숫자 표기법/집합 숫자 표기법.cc
+++ 백준/Silver/4848. 집합 숫자 표기법/집합 숫자 표기법.cc
@@ -10,44 +10,34 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n;
-    cin >> n;
+    int t;
+    cin >> t;
 
     unordered_map<int, string> i2s_map;
     unordered_map<string, int> s2i_map;
-    i2s_map.insert({0, "{}"});
-    s2i_map.insert({"{}", 0});
-    i2s_map.insert({1, "{{}}"});
-    s2i_map.insert({"{{}}", 1});
 
+    i2s_map[0] = "{}";
+    i2s_map[1] = "{{}}";
+    s2i_map[i2s_map[0]] = 0;
+    s2i_map[i2s_map[1]] = 1;
 
     for (int i = 2; i < 16; i++)
     {
-        string s = "{";
+        i2s_map[i] = "{";
         for (int j = 0; j < i; j++)
         {
-            s += i2s_map[j];
-            if (j != i - 1)
-                s += ",";
+            i2s_map[i] += i2s_map[j] + ",";
         }
-        s += "}";
-
-        i2s_map[i] = s;
-        s2i_map[s] = i;
+        i2s_map[i].back() = '}';
+        s2i_map[i2s_map[i]] = i;
     }
-
+    
     string a{}, b{};
-    for (int i = 0; i < n; i++)
+    while (t--)
     {
         cin >> a >> b;
-        
-        int a1{}, b1{};
-        a1 = s2i_map[a];
-        b1 = s2i_map[b];
-
-        cout << i2s_map[a1 + b1] << '\n';
+        cout << i2s_map[s2i_map[a] + s2i_map[b]] << '\n';
     }
-   
+
     return 0;
 }
-
