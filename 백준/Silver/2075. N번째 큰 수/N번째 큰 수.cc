e2ed4e8263b265006c301bee960740f197bc145a
JiHxxn-zip <kda2443@gmail.com>
2025-11-03 23:41:56 +0900

    [Silver III] Title: N번째 큰 수, Time: 312 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -12,20 +12,20 @@ int main()
 
     int n;
     cin >> n;
-
     priority_queue<int, vector<int>, greater<int>> pq;
 
-    int a{};
+    int num{};
     for (int i = 0; i < n*n; i++)
     {
-        cin >> a;
-        pq.push(a);
+        cin >> num;
+        pq.push(num);
 
         if (pq.size() > n)
             pq.pop();
     }
 
     cout << pq.top();
+
 
     return 0;
 }
