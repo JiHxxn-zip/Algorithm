c0b8f2335919984206ad399731bccae7b3f700ec
JiHxxn-zip <kda2443@gmail.com>
2025-12-01 18:57:21 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,51 +1,51 @@
-#include <iostream>
-#include "stack"
 using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <string>
 
-int main() 
+int main()
 {
-	stack<int> st;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-	string str;
-	int iInput(0), iInput2(0);
-	cin >> iInput;
+    int n;
+    cin >> n;
 
-	for (int i = 0; i < iInput; ++i)
-	{
-		cin >> str;
-		if (str == "push")
-		{
-			cin >> iInput2;
-			st.push(iInput2);
-		}
-		else if (str == "pop")
-		{
-			if (st.empty())
-				cout << "-1\n";
-			else
-			{
-				cout << st.top() << endl;
-				st.pop();
-			}
+    stack<int> s;
 
-		}
-		else if (str == "size")
-		{
-			cout << st.size() << endl;
-		}
-		else if (str == "empty")
-		{
-			if (st.empty())
-				cout << "1\n";
-			else
-				cout << "0\n";
-		}
-		else if (str == "top")
-		{
-			if (st.empty())
-				cout << "-1\n";
-			else
-				cout << st.top() << endl;
-		}
-	}
+    while (n--)
+    {
+        string a;
+        cin >> a;
+
+        if (a == "push")
+        {
+            string num;
+            cin >> num;
+            s.push(stoi(num));
+
+        }
+        else if (a == "pop")
+        {
+            int output = s.empty() ? -1 : s.top();
+            cout << output << '\n';
+
+            if(!s.empty())
+                s.pop();
+        }
+        else if (a == "size")
+            cout << s.size() << '\n';
+        else if (a == "empty")
+            cout << s.empty() << '\n';
+        else if(a=="top")
+        {
+            int output = s.empty() ? -1 : s.top();
+            cout << output << '\n';
+        }
+
+    }
+    return 0;
 }
