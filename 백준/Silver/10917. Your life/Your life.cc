46099df35d097df1b0513829ed2cbec3c8ccf838
JiHxxn-zip <kda2443@gmail.com>
2025-12-31 23:35:33 +0900

    [Silver II] Title: Your life, Time: 72 ms, Memory: 9116 KB -BaekjoonHub

---
+++ 백준/Silver/10917. Your life/Your life.cc
@@ -0,0 +1,54 @@
+using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <climits>
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> vec(n + 1);
+    vector<int> dist(n + 1, -1);
+
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        vec[a].push_back(b);
+        vec[b].push_back(a);
+    }
+
+    queue<int> q;
+    q.push(1);
+    
+    dist[1] = 0;
+
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+
+        for (auto next : vec[cur])
+        {
+            if (dist[next] != -1)
+                continue;
+
+            dist[next] = dist[cur] + 1;
+            q.push(next);
+        }
+       
+    }
+
+    cout << dist[n];
+
+    return 0;
+}
