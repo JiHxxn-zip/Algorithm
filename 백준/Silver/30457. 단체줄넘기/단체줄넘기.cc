bba7b31a9f72dde7c3e747fe71668a191f9cc873
JiHxxn-zip <kda2443@gmail.com>
2026-01-08 22:00:43 +0900

    [Silver V] Title: 단체줄넘기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/30457. 단체줄넘기/단체줄넘기.cc
+++ 백준/Silver/30457. 단체줄넘기/단체줄넘기.cc
@@ -18,15 +18,11 @@ int main()
     
     priority_queue<int, vector<int>, greater<int>> pq;
 
-    int mx{};
-
     for (int i = 0; i < n; i++)
     {
         int a;
         cin >> a;
         pq.push(a);
-
-        mx = max(mx, a);
     }
 
     int answer{};
@@ -50,4 +46,3 @@ int main()
 
     return 0;
 }
-
