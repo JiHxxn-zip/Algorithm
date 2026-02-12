569029aca358b0ee465a44219f899eb96724ad98
JiHxxn-zip <kda2443@gmail.com>
2026-02-12 15:13:00 +0900

    [Silver I] Title: W키가 빠진 성원이, Time: 192 ms, Memory: 10352 KB -BaekjoonHub

--- 백준/Silver/28471. W키가 빠진 성원이/W키가 빠진 성원이.cc
+++ 백준/Silver/28471. W키가 빠진 성원이/W키가 빠진 성원이.cc
@@ -3,69 +3,66 @@ using namespace std;
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <limits>
+#include <cctype> 
+#include <iomanip>
 #include <queue>
-#include <climits> // INT_MAX
 
 typedef long long ll;
 
 string board[2001];
+char vis[2001][2001];
 
-bool vis[2001][2001];
-int dx[7]{ 0,-1,1, -1,1,-1,1 }; // 하좌우 위왼 위오 아왼 아오
-int dy[7]{ -1,0,0,  1,1,-1,-1 };
-
+int dx[7]{ 0, -1, 1, -1, -1, 1, 1 };// 상,하,좌,우, 왼위, 왼아, 오위, 오아
+int dy[7]{ -1, 0, 0, 1, -1, 1, -1 };// 상,하,좌,우, 왼위, 왼아, 오위, 오아
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n;
+    cin >> n;
+   
+    pair<int, int> start{};
+    for (int i = 0; i < n; i++)
+    {
+        cin >> board[i];
 
-	int n;
-	cin >> n;
+        for (int j = 0; j < board[i].size(); j++)
+            if (board[i][j] == 'F')
+                start = {i, j};
+    }
 
-	for (int i = 0; i < n; i++)
-		cin >> board[i];
+    queue<pair<int, int>> q;
+    q.push(start);
+    vis[start.first][start.second] = 1;
 
+    int answer{};
+    while (!q.empty())
+    {
+        auto cur = q.front();
+        q.pop();
 
-	// . 이동가능 # 벽 F 도착지점
-	queue<pair<int, int>> q;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < board[i].size(); j++)
-			if (board[i][j] == 'F')
-			{
-				q.push({ i,j });
-				vis[i][j] = true;
-			}
-	
-	int answer{};
+        for (int i = 0; i < 7; i++)
+        {
+            int nx = dx[i] + cur.second;
+            int ny = dy[i] + cur.first;
 
-	while (!q.empty())
-	{
-		auto cur = q.front();
-		q.pop();
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+                continue;
+            if (vis[ny][nx] || board[ny][nx] != '.')
+                continue;
 
-		for (int i = 0; i < 7; i++)
-		{
-			int nx = dx[i] + cur.second;
-			int ny = dy[i] + cur.first;
+            vis[ny][nx] = 1;
+            q.push({ny, nx});
+            answer++;
+        }
+    }
 
-			if (nx < 0 || nx >= n || ny < 0 || ny >= n)
-				continue;
-			if (board[ny][nx] == '#')
-				continue;
-			if (vis[ny][nx]) 
-				continue;
+    cout << answer;
 
-			vis[ny][nx] = true;
-			q.push({ ny, nx });
-
-			if (board[ny][nx] == '.')
-				answer++;
-		}
-	}
-
-	cout << answer;
-
-	return 0;
+   
+    return 0;
 }
