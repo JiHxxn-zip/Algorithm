67ef65d23806b802067b4b7a688abe236efdb7e7
JiHxxn-zip <kda2443@gmail.com>
2026-02-23 15:29:57 +0900

    [Silver III] Title: 은?행 털!자 1, Time: 128 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/26267. 은？행 털！자 1/은？행 털！자 1.cc
+++ 백준/Silver/26267. 은？행 털！자 1/은？행 털！자 1.cc
@@ -3,32 +3,33 @@ using namespace std;
 #include <algorithm>
 #include <vector>
 #include <unordered_map>
-#include <string>
-#include <climits> // INT_MAX
-#include <iomanip> // 소숫점 제한
+#include <limits>
+#include <cctype> 
+#include <iomanip>
 
-using ll = long long;
+typedef long long ll;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+   
+    int n;
+    cin >> n;
 
-	int n;
-	cin >> n;
+    unordered_map<ll, ll> map;
+    ll answer{};
+    for (int i = 0; i < n; i++)
+    {
+        int x, t, c;
+        cin >> x >> t >> c;
 
-	unordered_map<ll, ll> map;
-	ll answer{};
-	for (int i = 0; i < n; i++)
-	{
-		int x, t, c;
-		cin >> x >> t >> c;
+        map[t - x] += c;
+        answer = max(answer, map[t - x]);
+    }
 
-		map[t - x] += c;
-		answer = max(answer, map[t-x]);
-	}
+    cout << answer;
 
-	cout << answer;
-	return 0;
+    return 0;
 }
