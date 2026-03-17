7946aa674869fd2e9dcd9b68717f7bbfae6a632b
JiHxxn-zip <kda2443@gmail.com>
2026-03-17 16:18:47 +0900

    [Silver I] Title: Pineapple Advertising, Time: 276 ms, Memory: 20176 KB -BaekjoonHub

--- 백준/Silver/17241. Pineapple Advertising/Pineapple Advertising.cc
+++ 백준/Silver/17241. Pineapple Advertising/Pineapple Advertising.cc
@@ -2,62 +2,64 @@ using namespace std;
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <string>
+#include <set>
 #include <limits>
-#include <cctype> 
-#include <iomanip>
+#include <iomanip> // 소숫점 제한
 
-typedef long long ll;
-
-vector<int> house[200001];
-int visit[200001] = { 0 };
-
-int delivery(int start) 
-{
-    if (visit[start] == -2) 
-        return 0;   // 이미 이 집에서 직접 광고함
-
-    int cnt = 0;
-
-    if (visit[start] == 0) 
-        cnt++;                  // 아직 광고 못 받았던 집이면 자기 자신 포함
-    
-    visit[start] = -2;          // 이제 이 집은 직접 광고한 집으로 처리
-
-    for (int i = 0; i < house[start].size(); i++) 
-    {
-        int a = house[start][i];
-        if (visit[a] == 0) 
-        {
-            cnt++;
-            visit[a] = -1;      // 광고 받음(직접 광고한 건 아님)
-        }
-    }
-    return cnt;
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    
+
     // 집, 길, 피자
     int n, m, q;
     cin >> n >> m >> q;
+
+    vector<vector<int>> vec(n + 1);
 
     for (int i = 0; i < m; i++)
     {
         int a, b;
         cin >> a >> b;
-        house[a].push_back(b);
-        house[b].push_back(a);
+        vec[a].push_back(b);
+        vec[b].push_back(a);
     }
+
+    vector<bool> vis(n + 1, false);
+    vector<bool> firstVis(n + 1, false);
 
     for (int i = 0; i < q; i++)
     {
-        int temp;
-        cin >> temp;
-        cout << delivery(temp) << '\n';
+        int s;
+        cin >> s;
+
+        if (firstVis[s])
+        {
+            cout << 0 << '\n';
+            continue;
+        }
+
+        firstVis[s] = true;
+        int count{};
+
+        if (!vis[s])
+        {
+            vis[s] = true;
+            count++;
+        }
+        for (auto& x : vec[s])
+        {
+            if (!vis[x])
+            {
+                vis[x] = true;
+                count++;
+            }
+        }
+        cout << count << '\n';
     }
+
     return 0;
 }
