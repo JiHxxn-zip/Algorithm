d409f67c51e62437d1c66107e67faf987b9abcad
JiHxxn-zip <kda2443@gmail.com>
2026-03-31 15:10:58 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.cc
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.cc
@@ -2,6 +2,7 @@ using namespace std;
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <stack>
 #include <limits>
 #include <cctype> 
 #include <iomanip>
@@ -12,27 +13,32 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
+
     int n;
     cin >> n;
 
-    vector<int> vec(n);
+    stack<int> s;
     for (int i = 0; i < n; i++)
     {
-        cin >> vec[i];
+        int x;
+        cin >> x;
+        s.push(x);
     }
 
     int count{};
-    for (int i = n-2; i >= 0; i--) // 뒤에서 부터 순회. 작게 빼는거
+    while (!s.empty())
     {
-        if (vec[i] >= vec[i + 1]) // 만약 뒤보다 크다면 줄여야하함
+        int cur = s.top();
+        s.pop();
+
+        if (!s.empty() && cur <= s.top())
         {
-            int add = vec[i] - vec[i + 1] + 1;
-            count += add;
-            vec[i] -= add;
+            int temp = s.top() - cur + 1;
+            count += temp;
+            s.top() -= temp;
         }
     }
-    cout << count;
 
+    cout << count;
     return 0;
 }
